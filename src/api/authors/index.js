4d4e73fb07cf2f5e3869dbcd616d81e24445b997
ArmndoHg <armndohernndez@gmail.com>
2022-12-21 16:56:15 +0100

    Finally finish!

--- src/api/authors/index.js
+++ src/api/authors/index.js
@@ -37,26 +37,54 @@ authorsRouter.get("/", (req, res) => {
 
 //2. Get single
 
-authorsRouter.get("/:authorId", (req, res) => {});
+authorsRouter.get("/:authorId", (req, res) => {
+  const authorId = req.params.authorId;
+  /*  console.log(authorId); */
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
+  const author = authorsArray.find((author) => author.id === authorId);
+  res.send(author);
+});
 
 //3. Post
 
 authorsRouter.post("/", (req, res) => {
   console.log("REQ BODY", req.body);
-  const newAuthor = { ...req.body, createdAt: new Date(), Id: uniqid() };
+  const newAuthor = { ...req.body, createdAt: new Date(), id: uniqid() };
   const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
   authorsArray.push(newAuthor);
   fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray));
   res.status(201).send({ id: newAuthor.id });
-  /* console.log(newAuthor); */
 });
 
 //4. Put
 
-authorsRouter.put("/:authorId", (req, res) => {});
+authorsRouter.put("/:authorId", (req, res) => {
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
+
+  const index = authorsArray.findIndex(
+    (author) => author.id === req.params.authorId
+  );
+  const oldAuthor = authorsArray[index];
+  const updatedAuthor = { ...oldAuthor, ...req.body, updatedAt: new Date() };
+  authorsArray[index] = updatedAuthor;
+
+  //saving
+  fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray));
+
+  res.send(updatedAuthor);
+});
 
 //5. Delete
 
-authorsRouter.delete("/:authorId", (req, res) => {});
+authorsRouter.delete("/:authorId", (req, res) => {
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
+  const remainingAuthors = authorsArray.filter(
+    (author) => author.id !== req.params.authorId
+  );
+
+  fs.writeFileSync(authorsJSONPath, JSON.stringify(remainingAuthors));
+
+  res.status(204).send("Now it's gone");
+});
 
 export default authorsRouter;
